bb722210f997d6a5a23489ff08a32edb7a3dc6bb
ordiaprecious <ordiaprecious@gmail.com>
2023-10-03 13:05:28 +0100

    my first commit

---
+++ currency.js
@@ -0,0 +1,13 @@
+function press(){
+    var show =(document.getElementById('start').value)*'965'
+    document.getElementById('screen').value=show 
+}
+function dollar(){
+    var so =(document.getElementById('start').value)/'965'
+    document.getElementById('screen').value= so
+}
+
+function comot(){
+    var done = document.getElementById('start').value =''
+    document.getElementById('screen').value= done
+}
